ea9b4048713e8cf726be6abe173fae77892149e6
Paulo Ferreira <40891723+paulo-jgf@users.noreply.github.com>
2021-03-25 14:17:54 -0300

    Adding test dummy

---
+++ test_module.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 14:14:59 2021
+
+@author: paulo
+"""
+
+def say_hello():
+    print('Hello world')
+    
+def inner_import():
+    import requests
